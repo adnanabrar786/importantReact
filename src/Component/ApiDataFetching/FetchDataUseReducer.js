1f81565df600b0bce5e2822f6e3c9ed80ff51074
adnanabrar786 <adnan1245abrar@gmail.com>
2022-11-18 15:18:44 -0800

    commit

--- src/Component/ApiDataFetching/FetchDataUseReducer.js
+++ src/Component/ApiDataFetching/FetchDataUseReducer.js
@@ -1,52 +1,51 @@
-import axios from 'axios';
-import React, { useEffect , useReducer } from 'react';
+import axios from "axios";
+import React, { useEffect, useReducer } from "react";
 
 const FetchDataUseReducer = () => {
+  const initialState = {
+    loading: true,
+    Post: {},
+    error: "",
+  };
 
-    const initialState = {
-        loading: true,
-        Post: {},
-        error: ""
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "FETCH_SUCESS":
+        return {
+          loading: false,
+          Post: action.payload,
+          error: "",
+        };
+      case "FETCH_ERROR":
+        return {
+          loading: false,
+          Post: {},
+          error: "something went wrong",
+        };
+      default:
+        return state;
     }
+  };
 
-    
-    const reducer = (state, action) => {
-        switch (action.type) {
-            case 'FETCH_SUCESS':
-                return {
-                    loading: false,
-                    Post: action.payload,
-                    error: ""
-                }
-            case 'FETCH_ERROR':
-                return {
-                    loading: false,
-                    Post: {},
-                    error: "something went wrong"
-                }
-            default:
-                return state
-        }
-    }
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-    const [state, dispatch] = useReducer(reducer, initialState)
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholde.typicode.com/posts/1")
+      .then((response) => {
+        dispatch({ type: "FETCH_SUCESS", payload: response.data });
+      })
+      .catch((error) => {
+        dispatch({ type: "FETCH_ERROR" });
+      });
+  }, []);
 
-    useEffect(() => {
-        axios.get("https://jsonplaceholde.typicode.com/posts/1")
-            .then(response => {
-                dispatch({ type: 'FETCH_SUCESS', payload: response.data })
-            })
-            .catch(error => {
-                dispatch({ type: 'FETCH_ERROR' })
-            })
-    }, []);
-
-    return (
-        <>
-            <label>{state.loading ? "loading" : state.Post.title}</label>
-            <label>{state.error ? state.error : null}</label>
-        </>
-    );
+  return (
+    <>
+      <label>{state.loading ? "loading" : state.Post.title}</label>
+      <label>{state.error ? state.error : null}</label>
+    </>
+  );
 };
 
 export default FetchDataUseReducer;
