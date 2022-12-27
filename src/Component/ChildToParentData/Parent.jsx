707728d01e76a55d5c70fa4bd70d23bc108fed97
adnanabrar786 <adnan1245abrar@gmail.com>
2022-12-27 10:43:36 -0800

    commit

--- src/Component/ChildToParentData/Parent.jsx
+++ src/Component/ChildToParentData/Parent.jsx
@@ -1,19 +1,23 @@
 // video ::::::::::::
 // https://www.youtube.com/watch?v=NMerkZt7iVw&t=3s 
 
-import React from 'react'
+import React, { useState } from 'react'
 import Child from './Child';
 
 const Parent = () => {
 
-    const getData = (data) => {
-        console.log(data);
-    }
+  const [showData, setShowData] = useState("")
+
+  const getData = (data) => {
+    console.log(data);
+    setShowData(data)
+  }
 
   return (
     <div>
       Parent
-      <Child getData={getData}/>
+      <Child getData={getData} />
+      <h3>{showData}</h3>
     </div>
   )
 }
